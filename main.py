0ea3ae36d4326a77b409e0bd81674e2606f8e2fb
nsmith <nathan.smith@aremedia.com.au>
2024-10-25 15:38:40 +1100

    Day 22: added apps description, images and links

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/photo.png", use_column_width="auto", caption="Selfie")
 
 
 with col2:
