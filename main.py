7bcb6d5b055167354888c9278ddceca3cc316569
nsmith <nathan.smith@aremedia.com.au>
2024-10-25 13:26:20 +1100

    Day 22: rendered app titles into columns.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
@@ -21,3 +22,15 @@ content2 = """
 Below you can find some apps I have built in Python. Feel free to contact me!
 """
 st.write(content2)
+
+col3, col4 = st.columns(2)
+
+data = pd.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in data[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in data[10:].iterrows():
+        st.header(row["title"])
