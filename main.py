892a3ff1218189ebb43611fbf94384946373a619
nsmith <nathan.smith@aremedia.com.au>
2024-10-25 13:10:56 +1100

    Add description on new columns.

--- main.py
+++ main.py
@@ -7,9 +7,17 @@ col1, col2 = st.columns(2)
 with col1:
     st.image("images/photo.png")
 
+
 with col2:
     st.title("Nathan Smith")
     content = """
-    Add text
+    Hi, I am Nathan. I am just starting out in my journey of becoming a Python programmer.
+    I want to learn all things Python to not limit myself.
     """
-    st.write(content)
+
+    st.info(content)
+
+content2 = """
+Below you can find some apps I have built in Python. Feel free to contact me!
+"""
+st.write(content2)
